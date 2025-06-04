a77541bdc170446be6d60593436563797d018a54
Aaditya Bhutada <53865945+adityabhutada@users.noreply.github.com>
2025-06-05 01:35:30 +0530

    Ensure username and password exist before checking

--- admin/auth.php
+++ admin/auth.php
@@ -1,5 +1,11 @@
 <?php
 require '../config.php';
+
+if (!isset($_POST['username'], $_POST['password'])) {
+    header('Location: login.php');
+    exit;
+}
+
 if ($_POST['username'] === ADMIN_USER && $_POST['password'] === ADMIN_PASS) {
     $_SESSION['admin'] = true;
     header("Location: dashboard.php");
